013d424a9e97a205052f4e5e64db26ffc93861f9
Dan Vera <dan@yovu.co>
2014-05-19 17:39:11 -0400

    structure

--- index.php
+++ index.php
@@ -2,4 +2,30 @@
   require 'vendor/autoload.php';
 
   $app->setName('blockmines-proxy');
+
+  //Routes
+	$app->get('/', 'home');
+
+  // API
+	$app->group('/api', function () use ($app) {
+
+    # v1
+		$app->group('/v1', function () use ($app) {
+
+      $app->group('/:api', function () use ($app) {
+
+      });
+
+    });
+
+  });
+
+  $app->contentType('application/json');
+
+  # lets go
+	$app->run();
+
+	function home() {
+		echo "You shall not pass";
+	}
 ?>
